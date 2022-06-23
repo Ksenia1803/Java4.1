563ec4519ed85554bd8372ac72cd5cd6212971ec
Ксения Тютчева <dmitrievna13@mail.ru>
2022-06-23 17:44:17 +0700

    Commit2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     //public static void main(String[] args) {
-       // int price = 65852;
-       // int mile = price / 20;
-       // System.out.println("Бонусные мили =" + mile);
+    // int price = 65852;
+    // int mile = price / 20;
+    // System.out.println("Бонусные мили =" + mile);
 
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
